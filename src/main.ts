aead0d62eed39dfe172d7eebf4c5abccc36a949b
Sattvik <sattvik@Sattviks-MacBook-Pro.local>
2025-03-26 12:49:21 +0530

    fix: basic impl

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
 
 /**
  * The main function for the action.
@@ -8,18 +7,14 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    // const pluginVersion: string = core.getInput('plugin-version')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('branches', {
+      'plugin-interface': '7.1',
+      postgresql: 'fix/test-speed',
+      mysql: '8.1',
+      mongodb: '1.31'
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
